ada82d7c63e3f9cdb70fc4130ece65f93e6e9cc4
Alexander <alex.m.sky@gmail.com>
2018-09-14 17:33:29 +0300

    резерв

--- gulpfile.js
+++ gulpfile.js
@@ -3,97 +3,26 @@ var gulp = require('gulp');
 
 // Stylus
 var stylus = require('gulp-stylus');
-
 gulp.task('styl', function () {
-  return gulp.src('app/assets/styles/styles.styl')
+  return gulp.src('app/styles/style.styl')
   .pipe(stylus())
   .pipe(gulp.dest('dist/assets/'))
 });
 
 // Pug
 var pug = require('gulp-pug');
-
 gulp.task('pug', function() {
-  return gulp.src('app/templates/pages/**/*.pug')
+  return gulp.src('app/pages/*.pug')
   .pipe(pug({
     pretty: true
   }))
-  .pipe(gulp.dest('dist/static/pages/'))
-});
-
-// Сервер (Medium)
-gulp.task('server', function() {
-  var express = require('express');
-  var app     = express();
-  var fs      = require('fs');
-  // отключаем кеширования
-  app.disable('view cache');
-  // указываем какой шаблонизатор использовать
-  app.set('view engine', 'pug');
-  // расположение шаблонов ('src/templates')
-  app.set('views', './');
-  // путь до наших стилей, картинок, скриптов и т.д.
-  app.use('/assets', express.static('./public/assets/**/*'));
-  // роутинг на наши страницы
-  app.get('/static/*.*', function(req, res) {
-    // регулярка для получения пути до шаблона
-    var fileName = req.url.replace(/static\/|\..*$/g, '') || 'home';
-    res.render('app/templates/' + fileName, {}); (1)
-  });
-  // редирект на главную страницу
-  app.get('/static', function(req, res) {
-    res.redirect('/static/pages/home.html');
-  });
-  var listener    = app.listen();
-  var port        = listener.address().port;
-  var browserSync = require('browser-sync').create();
-  // proxy на локальный сервер на Express
-  browserSync.init({
-    proxy: 'http://localhost:' + port,
-    startPath: '/static/',
-    notify: false,
-    tunnel: false,
-    host: 'localhost',
-    port: port,
-    logPrefix: 'Proxy to localhost:' + port,
-  });
-  // обновляем страницу, если обновились assets файлы
-  browserSync.watch('./dist/assets/**/*').on('change', browserSync.reload);
-  // обновляем страницу, если был изменен исходник шаблона
-  browserSync.watch('./app/templates/**/*').on('change', browserSync.reload);
-});
-
-// browserSync
-var browserSync = require('browser-sync').create();
-// Static server
-gulp.task('browser-sync', function() {
-    browserSync.init({
-        server: {
-            baseDir: "./dist/static/pages/"
-        }
-    });
+  .pipe(gulp.dest('dist/'))
 });
 
 // Watch
 var watch = require('gulp-watch');
 
 gulp.task('watch', function() {
-  gulp.watch('app/templates/pages/**/*.styl', ['styl']);
-  gulp.watch('app/templates/pages/**/*.pug', ['pug']);
-  // обновляем страницу, если обновились assets файлы
-  gulp.watch('app/templates/pages/**/*.styl').on('change', browserSync.reload);
-  // обновляем страницу, если был изменен исходник шаблона
-  gulp.watch('app/templates/pages/**/*.pug').on('change', browserSync.reload);
+  gulp.watch('app/**/*.styl', ['styl']);
+  gulp.watch('app/**/*.pug', ['pug']);
 });
-
-// gulpCopy
-// var gulpCopy = require('gulp-copy');
-// var sourceFiles = ['app/resources/images/*'];
-// var destination = 'dist/images/';
-//
-// gulp.task('gulpCopy', function() {
-//   return gulp
-//   .src(sourceFiles)
-//   .pipe(gulpCopy(outputPath, options))
-//   .dest(destination);
-// })
