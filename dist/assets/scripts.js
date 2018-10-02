ef25155143cde8a77acade0d45e5886c0ed680cf
Alexander <alex.m.sky@gmail.com>
2018-10-02 18:11:39 +0300

    Scroll Top - готово

--- dist/assets/scripts.js
+++ dist/assets/scripts.js
@@ -32,5 +32,25 @@ var activeMenu = () =>
 
 };
 activeMenu();
-
 // /активный пункт меню
+
+// Прокрутка к началу страницы
+$(document).ready(function(){
+
+		$('.scroll-to-top').fadeOut();
+    //Check to see if the window is top if not then display button
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('.scroll-to-top').fadeIn();
+        } else {
+            $('.scroll-to-top').fadeOut();
+        }
+    });
+
+    //Click event to scroll to top
+    $('.scroll-to-top').click(function(){
+        $('html, body').animate({scrollTop : 0},800);
+        return false;
+    });
+});
+// /Прокрутка к началу страницы
